f7d935b60e5e5f029dc63ec641903f43a6ffd8d8
RenePeuser - Rene Peuser <rene.peuser@hotmail.de>
2016-06-08 21:04:55 +0200

    startup

---
+++ MathExtension/MathExtensionApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MathExtension;
+
+namespace MathExtensionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = 1.Add(9).Subtract(8).Multiply(5).Divide(10);
+            Console.WriteLine(result);
+        }
+    }
+}
